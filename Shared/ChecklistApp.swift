a36e4d556d7d98b2bc9d5e95cb15fef302d25657
Jemil Pepena <jemil.pepena@griffithuni.edu.au>
2022-04-13 17:01:53 +1000

    Initial Commit

---
+++ Shared/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Shared
+//
+//  Created by Jemil Pepena on 13/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
