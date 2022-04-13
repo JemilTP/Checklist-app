ea6ac19640a9f2efdf08338702a27cc9eac3db7d
Jemil Pepena <jemil.pepena@griffithuni.edu.au>
2022-04-13 18:51:56 +1000

    text has been displayed and aligned correctly. Now needs check boxes.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack (alignment: .leading,spacing: 15){
+            Text("To-Do list")
+                .padding(.init(top: 30, leading: 30, bottom: 40, trailing: 30))
+                
+                .font(.largeTitle)
+                .frame(alignment: .leading)
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        
+        
+            Text("Clean room")
+                .padding(.leading, 30)
+                
+                
+            Text("Buy groceries")
+                .padding(.leading, 30)
+                
+            Text("Clean car")
+                .padding(.leading, 30)
+                
+            Text("Call Mum")
+                .padding(.leading, 30)
+               
+        }
+        .frame(minWidth: 0,  maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
