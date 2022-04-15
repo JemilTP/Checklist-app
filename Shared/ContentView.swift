0790ec4da7d982a9d7bf44599c939de7e9de1cd3
Jemil Pepena <jemil.pepena@griffithuni.edu.au>
2022-04-15 20:15:40 +1000

    milestone 1, static checklist

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,33 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack (alignment: .leading,spacing: 15){
+        VStack{
             Text("To-Do list")
-                .padding(.init(top: 30, leading: 30, bottom: 40, trailing: 30))
+                // .padding(.init(top: 30, leading: 30, bottom: 40, trailing: 30))
                 
                 .font(.largeTitle)
-                .frame(alignment: .leading)
+                .frame(width: 200,height: 100, alignment: .leading)
                 .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
+        HStack {
+            
+            VStack(alignment: .leading){
         
         
             Text("Clean room")
-                .padding(.leading, 30)
+                    .padding(.leading, 30) .padding(.bottom,10)
                 
                 
             Text("Buy groceries")
-                .padding(.leading, 30)
+                .padding(.leading, 30) .padding(.bottom,10)
                 
             Text("Clean car")
-                .padding(.leading, 30)
+                .padding(.leading, 30) .padding(.bottom,10)
                 
             Text("Call Mum")
-                .padding(.leading, 30)
+                .padding(.leading, 30) .padding(.bottom,10)
                
+        }
+            VStack{
+              //  Button("Done"){} .padding(.bottom, 10)
+                Button("Done"){}.padding(.bottom, 10)
+               // Button("Done"){}.padding(.bottom, 10)
+                Button("Done"){}.padding(.bottom, 10)
+            }.padding(.leading, 165)
         }
         .frame(minWidth: 0,  maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
     }
-        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
